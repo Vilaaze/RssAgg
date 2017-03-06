393f655ddd2747d555b449690ccef77c2f1bed07
Vilaaze <brian@reeder.com>
2017-03-06 11:16:36 -0600

    v0.1.2 Read JSON.txt Scans in all stories from JSON.txt Next task to strip away auxiliary verbs

--- read.py
+++ read.py
@@ -1,3 +1,22 @@
 
+import json
 
-print 'Hello World!'
+db = []
+try:
+	f_db = open("JSON.txt", "r")
+	for s in f_db:
+		db.append(json.loads(s))
+	
+	f_db.close()
+
+except:
+	print "No existing JSON"
+
+for story in db:
+	print '---- ---- ---- ---- ---- ---- ---- ---- ---- ---- ---- ----'
+	print story['title'], ':'
+	print
+
+	print story['summary']
+	print story['link']
+	print
