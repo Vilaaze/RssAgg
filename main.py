79158b07d8214cd0b8683daa32d8b2a777f65fe8
Vilaaze <brian@reeder.com>
2017-03-06 14:57:15 -0600

    v0.1.3 Function Refactoring Separated code into more functions for ease of readibility

--- main.py
+++ main.py
@@ -14,70 +14,132 @@
 import feedparser
 import json
 
-def rep_all(s, li, char):
+# # # # # # # # # # # # # 
+# Replace All Characters
+#
+# s  = string to be rewritten
+# li = list of characters to be replaced
+# c  = value to replace with 
+def replace_all(s, li, char):
 	for c in li:
 		s = s.replace(c, char)
-
 	return s
+# # # # # # # # # # # # # 
 
+# # # # # # # # # # # # # 
+# Assemble JSON
+def assemble_json(entry):
+
+		title = entry['title']
+		summary = entry['summary']
+
+		i = summary.find("<")
+		# If there is HTML in the summary, get rid of it
+		if(i != -1):
+			summary =  summary[0:i]
+		else:
+			summary =  summary
+		
+		# Peel off ' " so it doesnt disrupt the JSON
+		title = replace_all(title, ['\'', '"' ], '')
+		summary = replace_all(summary, ['\'', '"'], '')
+
+		
+		s_JSON = '{"id": "'+ entry['id']  +'", "title": "'+ title  +'", "summary": "'+ summary +'", "link": "'+ entry['link'] +'"}'
+
+		d_JSON = json.loads(s_JSON)
+		return d_JSON
+
+
+# # # # # # # # # # # # # 
+
+
+# # # # # # # # # # # # # 
+# Load JSON from file
+#
+# f_name = File Name of JSON database
+#
+# Open file
+# Iterate through file and read JSON
+def load_json(f_name):
+	try:
+		json_list = []
+		f_db = open(f_name, 'r')
+		for s in f_db:
+			json_list.append(json.loads(s))
+	
+		f_db.close()
+		return json_list
+
+	except:
+		print 'File ' + f_name + ' not found'
+		return -1
+# # # # # # # # # # # # # 
+
+
+# # # # # # # # # # # # # 
+# Load Links from file
+# 
+# f_name = Name of File to Read
+#
+# Reads file to end scanning in
+# links into an array
+#
+def load_links(f_name):
+	try:
+		f_open = open(f_name, 'r')
+		link_return = []
+		for link in f_open:
+			link_return.append(link.strip())
+	
+		f_open.close()
+		return link_return
+	except:
+		print 'File ' + f_name + ' Not Found'
+		return -1
+# # # # # # # # # # # # # 
+
+
+# # # # # # # # # # # # # 
+# Main
+# 
+# 1. Get List of RSS Feed Links
+# 2. Parse RSS feeds
+# 3. Compile JSON objects from Feeds
+# 4. Check to see if any new stories have been fetched
+#
 # Fetch all feed links from file
-file_feeds = open("feeds.txt", "r")
-feeds = [] 
+links = load_links('feeds.txt') 
 
 # Parse all feeds
-for s in file_feeds:
-	feeds.append( feedparser.parse(s) )
+feeds = []
+for s in links:
+	feeds.append(feedparser.parse(s))
 
-file_feeds.close()
-
-j_list = []
 # Iterate through all feeds
+j_list = []
 for d in feeds:
 	print d['feed']['title']
 	print d['feed']['link']
 	print d.feed.subtitle
-	print len(d['entries'])
-	index = 0
 
-	#  Assemble JSONs from all Stories from each feed
+	count = 0
 	for entry in d['entries']:
-#		print entry.viewkeys()
-
 		# Determine if there is a summary
 		i = entry.summary.find("<")
-		index = index + 1
 		#if there isn't a summary (i == 0) then move on
 		if(i != 0):
-			title = rep_all(entry.title, ['\'', '"' ], '')
-			
-			# If there is HTML in the summary, get rid of it
-			if(i != -1):
-				s =  entry.summary[0:i]
-			else:
-				s =  entry.summary
-			
-			# Peel off ' " so it doesnt disrupt the JSON
-			s = rep_all(s, ['\'', '"'], '')
+			d_json = assemble_json(entry)
+			j_list.append(d_json)
+			count = count + 1
 
-		# Form the string for the JSON
-		s_JSON = '{"id": "'+ entry.id  +'", "title": "'+ title  +'", "summary": "'+ s +'", "link": "'+entry.link+'"}'
-
-		d_JSON = json.loads(s_JSON)
-#		print "TITLE: " +  d_JSON['title']
-		j_list.append(d_JSON)
-
-db = []
-try:
-	f_db = open("JSON.txt", "r")
-	for s in f_db:
-		db.append(json.loads(s))
+	print str(count) + '/' + str(len(d['entries'])) + ' Story(s) detected'
+	print
 	
-	f_db.close()
-
-except:
-	print "No existing JSON"
+db = load_json('JSON.txt')
 
 f_json = open("JSON.txt", "a")
+count = 0
 for j in j_list:
 	exists = 0
 	for j2 in db:
@@ -85,8 +147,10 @@ for j in j_list:
 			exists = 1
 
 	if(exists == 0):
-#		print j['title']
 		f_json.write(json.dumps(j))
 		f_json.write('\n')
+		count = count + 1
 
 f_json.close()
+
+print str(count) + ' Story(s) saved'
